1fcd70430e05732c1bfb5e58a3782bec10779a2a
Ismael <iconchav@outlook.cl>
2021-11-04 23:44:22 -0300

    prueba pagina notificaciones

--- app/src/main/java/cl/duocuc/pry2111/alertamascota/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/cl/duocuc/pry2111/alertamascota/ui/notifications/NotificationsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginStart
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -30,11 +32,18 @@ class NotificationsFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val txtPrueba = TextView(context)
+        val txtpruebaid = View.generateViewId()
+        txtPrueba.text = "Texto de prueba"
+        txtPrueba.id = txtpruebaid
+        txtPrueba.textSize = 20F
+        val param = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        param.marginStart = 40
+        param.topMargin = 20
+        param.startToStart = binding.notifPage.id
+        param.topToTop = binding.notifPage.id
+        txtPrueba.layoutParams = param
+        binding.notifPage.addView(txtPrueba)
         return root
     }
 
