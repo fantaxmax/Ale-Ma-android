d9c61f4bc96f8e4b30f513b82f2b4b83821a986b
Ismael <iconchav@outlook.cl>
2021-11-05 21:40:00 -0300

    correciones y diseño de pagina notficaciones

--- app/src/main/java/cl/duocuc/pry2111/alertamascota/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/cl/duocuc/pry2111/alertamascota/ui/notifications/NotificationsFragment.kt
@@ -1,17 +1,23 @@
 package cl.duocuc.pry2111.alertamascota.ui.notifications
 
+import android.icu.lang.UCharacter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.get
 import androidx.core.view.marginStart
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import cl.duocuc.pry2111.alertamascota.R
 import cl.duocuc.pry2111.alertamascota.databinding.FragmentNotificationsBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class NotificationsFragment : Fragment() {
 
@@ -32,23 +38,61 @@ class NotificationsFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val txtPrueba = TextView(context)
-        val txtpruebaid = View.generateViewId()
-        txtPrueba.text = "Texto de prueba"
-        txtPrueba.id = txtpruebaid
-        txtPrueba.textSize = 20F
-        val param = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,ConstraintLayout.LayoutParams.WRAP_CONTENT)
-        param.marginStart = 40
-        param.topMargin = 20
-        param.startToStart = binding.notifPage.id
-        param.topToTop = binding.notifPage.id
-        txtPrueba.layoutParams = param
-        binding.notifPage.addView(txtPrueba)
+
+        for(i in 1..15) {
+            binding.notifPage.addView(createNotifView("Notificacion de Prueba: "+i,"Esta es una notificacion creada en codigo",Date()))
+         }
+
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun createNotifView(title: String, content: String, date: Date): LinearLayout? {
+        val layout = LinearLayout(context)
+        layout.id = View.generateViewId()
+        layout.orientation = LinearLayout.VERTICAL
+        layout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+            marginStart = 20
+            topMargin = 16
+        }
+        val txtTitle = TextView(context)
+        txtTitle.id = View.generateViewId()
+        txtTitle.text = title
+        txtTitle.textSize = 20F
+        val txtDate = TextView(context)
+        txtDate.id = View.generateViewId()
+        txtDate.text = date.toString()
+        txtDate.textSize = 16F
+        val txtContent = TextView(context)
+        txtContent.id = View.generateViewId()
+        txtContent.text = content
+        txtContent.textSize = 18F
+        txtTitle.layoutParams =
+            LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+                .apply {
+            marginStart = 20
+            topMargin = 20
+        }
+        txtContent.layoutParams =
+            LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply{
+                marginStart = 20
+                topMargin = 20
+            }
+        txtDate.layoutParams =
+            LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply{
+                marginEnd = 20
+                topMargin = 0
+                marginStart = 20
+            }
+        layout.addView(txtTitle)
+        layout.addView(txtDate)
+        layout.addView(txtContent)
+
+        return layout
     }
 }
